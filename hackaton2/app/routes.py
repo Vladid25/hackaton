f341fd6c324f2d85dd407c2b45d957df44cac042
Vlad Vasiliev <v14d06@gmail.com>
2024-06-01 11:10:33 +0300

    changing reading file

--- hackaton2/app/routes.py
+++ hackaton2/app/routes.py
@@ -7,26 +7,17 @@ from .utils.feature_importance import feature_importance
 
 @app.route('/')
 def index():
-    data = pd.read_excel('hackaton/hackaton2/app/static/uploads/Online Sales Data.xlsx')
+    data = pd.read_excel('hackaton/hackaton2/app/static/uploads/dataset.xlsx')
 
-    # Вибірка перших 5 рядків даних
     sample_data = data.head(5).to_html(classes='table table-striped', index=False)
 
-    # Аналітика даних
     missing_values, data_description = analyze_data(data)
 
-    # Форматування результатів аналітики
-    analysis = f"Missing Values:<br>{missing_values.to_frame().to_html(classes='table table-striped')}<br><br>Data Description:<br>{data_description.to_html(classes='table table-striped')}"
-
-    # Виявлення викидів
+    analysis = f"Missing Values:<br>{pd.DataFrame(missing_values).to_html(classes='table table-striped')}<br><br>Data Description:<br>{pd.DataFrame(data_description).to_html(classes='table table-striped')}"
     outliers = detect_outliers(data)
-    outliers_text = f"Outliers detected:<br>{outliers.to_html(classes='table table-striped')}"
-
-    # Візуалізація даних
+    outliers_text = f"Outliers detected:<br>{pd.DataFrame(outliers).to_html(classes='table table-striped')}"
     visualize_data(data)
 
-    # Визначення впливовості факторів
-    feature_importance_values = feature_importance(data)
-    feature_importance_text = f"Feature Importance:<br>{feature_importance_values.to_frame().to_html(classes='table table-striped')}"
-
-    return render_template('index.html', sample_data=sample_data, analysis=analysis, outliers_text=outliers_text, feature_importance_text=feature_importance_text)
+    #feature_importance_values = feature_importance(data)
+    #feature_importance_text = f"Feature Importance:<br>{pd.DataFrame(feature_importance_values).to_html(classes='table table-striped')}"
+    return render_template('index.html', sample_data=sample_data, analysis=analysis, outliers_text=outliers_text)
--- hackaton2/app/utils/visualization.py
+++ hackaton2/app/utils/visualization.py
@@ -3,28 +3,44 @@ import seaborn as sns
 import plotly.express as px
 
 def visualize_data(data):
-    # Візуалізація розподілу даних
-    data.hist(bins=50, figsize=(20, 15))
-    plt.savefig('hackaton/hackaton2/app/static/images/histogram.png')
-    plt.close()
+    # Визуалізація розподілу даних
+    numeric_data = data.select_dtypes(include=['float64', 'int64'])
+    if not numeric_data.empty:
+        numeric_data.hist(bins=50, figsize=(20, 15))
+        plt.savefig('hackaton/hackaton2/app/static/images/histogram.png')
+        plt.close()
+    else:
+        print("No numerical columns found for histogram visualization.")
+
     # Boxplot для кожної числової колонки
-    data.select_dtypes(include=['float64', 'int64']).plot(kind='box', subplots=True, layout=(2, 3), figsize=(15, 10), title='Boxplot for Numerical Columns')
-    plt.savefig('hackaton/hackaton2/app/static/images/boxplot.png')
-    plt.close()
+    numeric_data = data.select_dtypes(include=['float64', 'int64'])
+    if not numeric_data.empty:
+        numeric_data.plot(kind='box', subplots=True, layout=(2, 3), figsize=(15, 10), title='Boxplot for Numerical Columns')
+        plt.savefig('hackaton/hackaton2/app/static/images/boxplot.png')
+        plt.close()
+    else:
+        print("No numerical columns found for boxplot visualization.")
 
     # Видалення нечислових даних для кореляційної матриці
     numeric_data = data.select_dtypes(include=['float64', 'int64'])
-    # Кореляційна матриця
-    plt.figure(figsize=(15, 10))
-    sns.heatmap(numeric_data.corr(), annot=True, fmt='.2f', cmap='coolwarm')
-    plt.title('Correlation Matrix')
-    plt.savefig('hackaton/hackaton2/app/static/images/correlation_matrix.png')
-    plt.close()
-    # Розподіл кожної змінної
-    for column in numeric_data.columns:
-        plt.figure(figsize=(10, 6))
-        sns.histplot(data[column].dropna(), kde=True)
-        plt.title(f'Distribution of {column}')
-        plt.savefig(f'hackaton/hackaton2/app/static/images/distribution_{column}.png')
+    if not numeric_data.empty:
+        # Кореляційна матриця
+        plt.figure(figsize=(15, 10))
+        sns.heatmap(numeric_data.corr(), annot=True, fmt='.2f', cmap='coolwarm')
+        plt.title('Correlation Matrix')
+        plt.savefig('hackaton/hackaton2/app/static/images/correlation_matrix.png')
         plt.close()
-    
+    else:
+        print("No numerical columns found for correlation matrix visualization.")
+
+    # Розподіл кожної змінної
+    numeric_data = data.select_dtypes(include=['float64', 'int64'])
+    if not numeric_data.empty:
+        for column in numeric_data.columns:
+            plt.figure(figsize=(10, 6))
+            sns.histplot(data[column].dropna(), kde=True)
+            plt.title(f'Distribution of {column}')
+            plt.savefig(f'hackaton/hackaton2/app/static/images/distribution_{column}.png')
+            plt.close()
+    else:
+        print("No numerical columns found for distribution visualization.")
